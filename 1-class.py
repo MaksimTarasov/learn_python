7820cb066f99c074209b6a0745007b6221f8883b
Maksim <maksimtarasov1981@gmail.com>
2020-11-28 20:53:13 +0200

    new class

--- 1-class.py
+++ 1-class.py
@@ -1,8 +1,38 @@
-class Fish():
+import pygame as pg
+
+class Fish:
+    def __init__(self,mass,age,health,name):
+        #fish birth )
+        #mass - mass of fish
+        #age - age of fish
+        #health - helth of fish
+        self.mass = mass
+        self.age = age
+        self.health = health
+        self.name = name
+       
+    def eat(self):
+        # all fish can eat)
+        pass
+    
     def swim(self):
         print('i can swim')
+    
+    def fish_dead(self):
+        #fish dead
+        pass
 
-bream = Fish()
+class Bream(Fish):
+    pass
+class Roach(Fish):
+    pass
+class Skremers(Fish):
+    pass
 
-bream.swim()
+
+b = Bream()
+
+
+
+
     
