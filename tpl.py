f1191c80599965572234ee42beff38f85c284eb2
Maksim <maksimtarasov1981@gmail.com>
2020-12-03 23:56:56 +0200

    add

--- tpl.py
+++ tpl.py
@@ -5,3 +5,10 @@ print (fishing_pl[2])
 (a,b)= fishing_pl[2]
 print('{:15} | {:^9} | {:9}'.format('','lat.','long.'))
 print('You fishing on river {0:15}, city {1:9} with coords: {2:8},{3:8}'.format(fishing_pl[0],fishing_pl[1],a,b))
+
+message_for_you = ('abrakadabra')
+sr = message_for_you[1:5:2]
+print(sr)
+sr2 = slice(1,5)
+print(message_for_you[sr2])
+print(message_for_you.split('a')[1:])
